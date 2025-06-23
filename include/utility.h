24616dc93ab644941a84c25714c9bc6a9aee6244
Jad haj mustafa <jhajmustafa@lely.com>
2025-06-23 11:35:23 +0200

    ros2

--- include/utility.h
+++ include/utility.h
@@ -1,21 +1,17 @@
-  #include<ndt.h>
-  
-  void debugPoseMarker(geometry_msgs::Pose p)
-  {
-    visualization_msgs::Marker marker;
+#include <visualization_msgs/msg/marker.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+static void debugPoseMarker(const geometry_msgs::msg::Pose &p, rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr debug_init_pose_pub)
+{
+    visualization_msgs::msg::Marker marker;
     marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time();
+    marker.header.stamp = rclcpp::Clock().now();
     marker.ns = "my_namespace";
     marker.id = 0;
-    marker.type = visualization_msgs::Marker::ARROW;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.position.x = p.position.x;
-    marker.pose.position.y = p.position.y;
-    marker.pose.position.z = p.position.z;
-    marker.pose.orientation.x = p.orientation.x;
-    marker.pose.orientation.y = p.orientation.y;
-    marker.pose.orientation.z = p.orientation.z;
-    marker.pose.orientation.w = p.orientation.w;
+    marker.type = visualization_msgs::msg::Marker::ARROW;
+    marker.action = visualization_msgs::msg::Marker::ADD;
+    marker.pose = p;
     marker.scale.x = 1;
     marker.scale.y = 0.1;
     marker.scale.z = 0.1;
@@ -23,5 +19,5 @@
     marker.color.r = 0.0;
     marker.color.g = 1.0;
     marker.color.b = 0.0;
-    debug_init_pose_pub.publish(marker);
-  }
+    debug_init_pose_pub->publish(marker);
+}
--- nodes/voxel_grid_filter.cpp
+++ nodes/voxel_grid_filter.cpp
@@ -1,6 +1,9 @@
-#include <ros/ros.h>
-#include <sensor_msgs/PointCloud2.h>
-
+#include <memory>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/voxel_grid.h>
@@ -10,112 +13,90 @@
 
 #define MAX_MEASUREMENT_RANGE 120.0
 
-ros::Publisher filtered_points_pub;
+class VoxelGridFilterNode : public rclcpp::Node {
+public:
+    VoxelGridFilterNode() : Node("voxel_grid_filter") {
+        this->declare_parameter<std::string>("points_topic", "/points_raw");
+        this->declare_parameter<double>("leaf_size", 2.0);
+        this->declare_parameter<double>("measurement_range", MAX_MEASUREMENT_RANGE);
+        this->declare_parameter<bool>("output_log", false);
 
-// Leaf size of VoxelGrid filter.
-static double voxel_leaf_size = 2.0;
+        this->get_parameter("points_topic", points_topic_);
+        this->get_parameter("leaf_size", voxel_leaf_size_);
+        this->get_parameter("measurement_range", measurement_range_);
+        this->get_parameter("output_log", output_log_);
 
-static bool _output_log = false;
-static std::ofstream ofs;
-static std::string filename;
+        filtered_points_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/filtered_points", 10);
+        scan_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+            points_topic_, 10, std::bind(&VoxelGridFilterNode::scan_callback, this, std::placeholders::_1));
 
-static std::string POINTS_TOPIC;
-static double measurement_range = MAX_MEASUREMENT_RANGE;
-
-static void scan_callback(const sensor_msgs::PointCloud2::ConstPtr& input)
-{
-  pcl::PointCloud<pcl::PointXYZ> scan;
-  pcl::fromROSMsg(*input, scan);
-
-  if(measurement_range != MAX_MEASUREMENT_RANGE){
-    scan = removePointsByRange(scan, 0, measurement_range);
-  }
-
-  pcl::PointCloud<pcl::PointXYZ>::Ptr scan_ptr(new pcl::PointCloud<pcl::PointXYZ>(scan));
-  pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_scan_ptr(new pcl::PointCloud<pcl::PointXYZ>());
-
-  sensor_msgs::PointCloud2 filtered_msg;
-
-  // if voxel_leaf_size < 0.1 voxel_grid_filter cannot down sample (It is specification in PCL)
-  if (voxel_leaf_size >= 0.1)
-  {
-    // Downsampling the velodyne scan using VoxelGrid filter
-    pcl::VoxelGrid<pcl::PointXYZ> voxel_grid_filter;
-    voxel_grid_filter.setLeafSize(voxel_leaf_size, voxel_leaf_size, voxel_leaf_size);
-    voxel_grid_filter.setInputCloud(scan_ptr);
-    voxel_grid_filter.filter(*filtered_scan_ptr);
-
-    
-    // define the heading direction
-    pcl::PointXYZ x_axis(1,0,0);
-
-    // keep front view points
-    pcl::PointIndices::Ptr front_index(new pcl::PointIndices());
-    pcl::ExtractIndices<pcl::PointXYZ> extract;
-
-    ROS_INFO("before filtering %i", (*filtered_scan_ptr).size());
-
-    for(int i=0; i<(*filtered_scan_ptr).size(); i++) {
-      auto point = filtered_scan_ptr->points[i];
-      double mod = sqrt(point.x*point.x + point.y*point.y);
-      point.x /= mod;
-      point.y /= mod;
-      double angle = acos(point.x*x_axis.x + point.y*x_axis.y);
-      // std::cout << "angle: " << angle << std::endl;
-
-      if(abs(angle)>M_PI/180.0*60) {
-        front_index->indices.push_back(i);
-        //std::cout << "added ";
-      }
+        RCLCPP_INFO(this->get_logger(), "Voxel leaf size is: %f", voxel_leaf_size_);
     }
 
-    extract.setInputCloud(filtered_scan_ptr);
-    extract.setIndices(front_index);
-    extract.setNegative(true);
-    //extract.filter(*filtered_scan_ptr);
+private:
+    void scan_callback(const sensor_msgs::msg::PointCloud2::SharedPtr input) {
+        pcl::PointCloud<pcl::PointXYZ> scan;
+        pcl::fromROSMsg(*input, scan);
 
-    ROS_INFO("after filtering %i", (*filtered_scan_ptr).size()); 
+        if (measurement_range_ != MAX_MEASUREMENT_RANGE) {
+            scan = removePointsByRange(scan, 0, measurement_range_);
+        }
 
-    pcl::toROSMsg(*filtered_scan_ptr, filtered_msg);
-  }
-  else
-  {
-    pcl::toROSMsg(*scan_ptr, filtered_msg);
-  }
+        pcl::PointCloud<pcl::PointXYZ>::Ptr scan_ptr(new pcl::PointCloud<pcl::PointXYZ>(scan));
+        pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_scan_ptr(new pcl::PointCloud<pcl::PointXYZ>());
+        sensor_msgs::msg::PointCloud2 filtered_msg;
 
-  filtered_msg.header = input->header;
-  filtered_points_pub.publish(filtered_msg);
+        if (voxel_leaf_size_ >= 0.1) {
+            pcl::VoxelGrid<pcl::PointXYZ> voxel_grid_filter;
+            voxel_grid_filter.setLeafSize(voxel_leaf_size_, voxel_leaf_size_, voxel_leaf_size_);
+            voxel_grid_filter.setInputCloud(scan_ptr);
+            voxel_grid_filter.filter(*filtered_scan_ptr);
 
-}
+            // define the heading direction
+            pcl::PointXYZ x_axis(1, 0, 0);
+            pcl::PointIndices::Ptr front_index(new pcl::PointIndices());
+            pcl::ExtractIndices<pcl::PointXYZ> extract;
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "voxel_grid_filter");
+            RCLCPP_INFO(this->get_logger(), "before filtering %zu", filtered_scan_ptr->size());
 
-  ros::NodeHandle nh;
-  ros::NodeHandle private_nh("~");
+            for (size_t i = 0; i < filtered_scan_ptr->size(); i++) {
+                auto point = filtered_scan_ptr->points[i];
+                double mod = sqrt(point.x * point.x + point.y * point.y);
+                if (mod == 0) continue;
+                point.x /= mod;
+                point.y /= mod;
+                double angle = acos(point.x * x_axis.x + point.y * x_axis.y);
+                if (fabs(angle) > M_PI / 180.0 * 60) {
+                    front_index->indices.push_back(i);
+                }
+            }
 
-  private_nh.getParam("points_topic", POINTS_TOPIC);
-  private_nh.getParam("output_log", _output_log);
+            extract.setInputCloud(filtered_scan_ptr);
+            extract.setIndices(front_index);
+            extract.setNegative(true);
+            //extract.filter(*filtered_scan_ptr); // Uncomment if you want to actually filter
 
-  private_nh.param<double>("leaf_size", voxel_leaf_size, 2.0);
-  ROS_INFO_STREAM("Voxel leaf size is: "<<voxel_leaf_size);
-  if(_output_log == true){
-	  char buffer[80];
-	  std::time_t now = std::time(NULL);
-	  std::tm *pnow = std::localtime(&now);
-	  std::strftime(buffer,80,"%Y%m%d_%H%M%S",pnow);
-	  filename = "voxel_grid_filter_" + std::string(buffer) + ".csv";
-	  ofs.open(filename.c_str(), std::ios::app);
-  }
+            RCLCPP_INFO(this->get_logger(), "after filtering %zu", filtered_scan_ptr->size());
+            pcl::toROSMsg(*filtered_scan_ptr, filtered_msg);
+        } else {
+            pcl::toROSMsg(*scan_ptr, filtered_msg);
+        }
 
-  // Publishers
-  filtered_points_pub = nh.advertise<sensor_msgs::PointCloud2>("/filtered_points", 10);
+        filtered_msg.header = input->header;
+        filtered_points_pub_->publish(filtered_msg);
+    }
 
-  // Subscribers
-  ros::Subscriber scan_sub = nh.subscribe(POINTS_TOPIC, 10, scan_callback);
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr filtered_points_pub_;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr scan_sub_;
+    std::string points_topic_;
+    double voxel_leaf_size_ = 2.0;
+    double measurement_range_ = MAX_MEASUREMENT_RANGE;
+    bool output_log_ = false;
+};
 
-  ros::spin();
-
-  return 0;
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<VoxelGridFilterNode>());
+    rclcpp::shutdown();
+    return 0;
 }
